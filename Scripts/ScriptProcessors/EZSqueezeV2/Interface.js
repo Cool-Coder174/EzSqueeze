ab6af3a6cbf41ad7f23f907b6bf0f01bb3b9fba6
Isaac <Isaachernandez174@gmail.com>
2025-10-13 14:16:59 -0700

    Initial commit

---
+++ Scripts/ScriptProcessors/EZSqueezeV2/Interface.js
@@ -0,0 +1,28 @@
+Content.makeFrontInterface(800, 600);
+
+/* I havent started scripting anything yet, just whipped up a rough prototype going. 
+Definitely need to redo the graphics to get them cleaner but its a start. 
+Added a basic dynamics module, it has the compressor inside and the knobs are linked.
+*/
+
+function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
